f135ef3933de70b5b6c47b1dddd80f4937b9c616
Hunter Braun <hunter.braun@gmail.com>
2016-04-27 22:10:19 -0500

    Add initial list creation via AJAX

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,28 @@
+class ListsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @lists = current_user.lists.all
+  end
+
+  def create
+    @list = current_user.lists.create(list_params)
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js {}
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :description)
+  end
+end
