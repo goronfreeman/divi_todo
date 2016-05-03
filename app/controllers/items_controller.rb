fd5ee149421bc9e3619d250fa8ff20357d9ba613
Hunter Braun <hunter.braun@gmail.com>
2016-05-03 01:36:33 -0500

    Refactor controllers and views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,24 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @items = current_user.items.all
+  def create
+    @list = List.find(params[:list_id])
+    @item = @list.items.build(item_params)
+
+    if @item.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js {}
+      end
+    else
+      # Do stuff
+    end
   end
 
-  def create
-    @item = Item.create(item_params)
-    @item.list_id = params[:list_id]
-
-    @item.save
+  def update
+    @list = List.find(:params[:challenge_id])
+    @task = Task.find(params[:id])
+    @task.update_attributes(item_params)
 
     respond_to do |format|
       format.html { redirect_to root_path }
@@ -30,6 +39,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :state, :list_id)
+    params.require(:item).permit(:title, :description, :state)
   end
 end
